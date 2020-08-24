dd8e8898b69f42749e6f3de87dc94d31fd720691
Bakdaulet <baidildaevbahon08@gmail.com>
2020-08-24 16:50:16 +0600

    first commit

---
+++ sc.js
@@ -0,0 +1,38 @@
+"use strict";
+ 
+
+/*function ork(){
+
+}
+let c = 10;
+const p =45;
+const start = document.querySelector('#sta');
+let alert4 = 4;
+
+console.log(name);
+var name = 'alex';
+
+
+const answear = [];
+answear[0]=prompt("hos are you", '');
+answear[1]=+prompt("how old are you", '');
+answear[2]=prompt("What is your name?", '');
+
+console.log(answear);
+*/
+
+//-------8 lesson------------interpoiation
+/*const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);*/
+
+//---------9 lesson----------operation
+//префиксный + и постфиксный: перфиксный деген алдына ++ или -- койылады. постфиксный деген артына ++ или -- койылады.
+//РАЗНИЦА: постфиксный форма старый значениесин биринши есептеиди
+let incr = 10, decr = 10;
+//incr++;
+//decr--;
+console.log(incr);
+console.log(decr);
+//------------10 lesson --------------- Git
+let f;
+ 
