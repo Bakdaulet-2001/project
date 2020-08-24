f3326aea5047f5fa55b076009bc07b99ab2c5a87
Bakdaulet <baidildaevbahon08@gmail.com>
2020-08-25 00:06:24 +0600

    Added standard HTML page tags

--- sc.js
+++ sc.js
@@ -21,18 +21,42 @@ answear[2]=prompt("What is your name?", '');
 console.log(answear);
 */
 
-//-------8 lesson------------interpoiation
+    //-------8 lesson------------interpoiation
 /*const category = 'toys';
 console.log(`https://someurl.com/${category}/5`);*/
 
-//---------9 lesson----------operation
+    //---------9 lesson----------operation
 //префиксный + и постфиксный: перфиксный деген алдына ++ или -- койылады. постфиксный деген артына ++ или -- койылады.
 //РАЗНИЦА: постфиксный форма старый значениесин биринши есептеиди
-let incr = 10, decr = 10;
+/*let incr = 10, decr = 10;
 //incr++;
 //decr--;
 console.log(incr);
-console.log(decr);
-//------------10 lesson --------------- Git
-let f;
- 
+console.log(decr);*/
+
+    //------------10 lesson --------------- Git
+//let f;
+
+    //----------12 lesson ------------ pracrick
+
+    /*let numberOfFilms = prompt("How many movies have you whatced? ",'');
+
+    const personalMovieDB = {
+        count:numberOfFilms,
+        movies: {},
+        actors:{},
+        genres:[],
+        private: false
+    };
+    console.log(personalMovieDB.count);
+    console.log(personalMovieDB);
+    let nn = prompt("one of the most recently viewed movies?", '');
+    let nn2 = prompt("how much do you rate it", '');
+
+    let movies ={
+       
+    };
+    movies[nn] = nn2;
+    console.log(movies);*/
+    
+    
